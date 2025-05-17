d89dc280ce7cd6a20911b813fdba8d9082094afc
Tarun-Chaturvedi <unfrost151@gmail.com>
2025-05-17 12:13:39 +0530

    another dsa and comment added

--- pattern/dsa2.cpp
+++ pattern/dsa2.cpp
@@ -1,13 +1,22 @@
+/* for pattern
+* 	*
+* 	* *
+* 	* * *
+* 	* * * *
+* 	* * * * *
+*/
+
 #include<iostream>
 using namespace std;
 int main(){
 	int x;
 	cout << "Enter a number:";
 	cin >> x;
-	for ( int i = 1 ; i <= x ; i++ ){
-      		for( int j = 1 ; j <=i ; j++ ){
+	for ( int i = 0 ; i <= x ; i++ ){
+      		for( int j = 0 ; j <=i ; j++ ){
 			cout << "x ";
 		}
 		cout << endl;
 	}
+	return 0;
 }
