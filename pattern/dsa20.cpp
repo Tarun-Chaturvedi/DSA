7608dbf562950a74fe7f0733bedc34ff80f26fad
Tarun-Chaturvedi <unfrost151@gmail.com>
2025-06-04 19:20:14 +0530

    another dsa

--- pattern/dsa20.cpp
+++ pattern/dsa20.cpp
@@ -16,18 +16,32 @@ int main(){
 	int a,b;
 	cout << "enter a number:";
 	cin >> b;
-	if(b%2!=0){b++;}
-	a = b/2;
-	for( int i = 1 ; i <= b ; i++ ){
+	if( b % 2 != 0 ){
+		b++;
+	}
+	a = b/2 ;
+	for(int i = 1 ; i <= b ; i++ ){
 		if(i < b/2){
-			for(int j = 1 ; j <= i ; j++ ){cout << "x";}
-			for(int k = 1 ; k <= 2*(b/2-i) ; k++ ){cout << " ";}
-			for(int l = 1 ; l <=i ; l++ ){cout << "x";}
+			for(int j = 1 ; j <= i ; j++ ){
+				cout << "x";
+			}
+			for(int k = 1 ; k <= 2*(b/2-i) ; k++ ){
+				cout << " ";
+			}
+			for(int l = 1 ; l <=i ; l++ ){
+				cout << "x";
+			}
 		}
 		else{
-			for(int j = 1 ; j <= b-i ; j++ ){cout << "x";}
-			for(int k = 1 ; k <= 2*(i-b/2) ; k++ ){cout << " ";}
-			for(int l = 1 ; l <= b-i ; l++ ){cout << "x";}
+			for(int j = 1 ; j <= b-i ; j++ ){
+				cout << "x";
+			}
+			for(int k = 1 ; k <= 2*(i-b/2) ; k++ ){
+				cout << " ";
+			}
+			for(int l = 1 ; l <= b-i ; l++ ){
+				cout << "x";
+			}
 		}
 		cout << endl;
 	}
