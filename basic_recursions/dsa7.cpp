74e54bed618bd51581917cb34d3dc64d98ab9f11
Tarun-Chaturvedi <unfrost151@gmail.com>
2025-07-18 13:35:59 +0530

    another recurse

--- basic_recursions/dsa7.cpp
+++ basic_recursions/dsa7.cpp
@@ -1,1 +1,34 @@
+/*	here is the question
+*		reversing an array
+*/
 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int &a,int &b){
+	int c = b;
+	b = a ;
+	a = c;
+}
+
+void rec(int start,int end,int a[]){
+	if(start >= end) return;
+	swap(a[start],a[end]);
+	rec(start+1,end-1,a);
+}
+
+int main(){
+	int n;
+	cout << "Enter the number of element in an array: ";
+	cin >> n ;
+	int a[n];
+	cout << "Enter the elements:" << endl;
+	for(int i = 0; i < n ;i++){
+		cin >> a[i];
+	}
+	rec(0,n-1,a);
+	cout << "Reversed Array:" <<endl;
+	for(int i=0;i<n;i++){ cout << a[i] << " ";}
+	return 0;
+}
